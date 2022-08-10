9f47811ae9cf22d5728a79c89a854ed2ede71969
Gerrit Aulmann <g.aulmann@carat.de>
2022-08-10 07:33:56 +0200

    first commit

---
+++ MoviesAPI/Data/Movie.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MoviesAPI.Data
+{
+    public class Movie
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Summary { get; set; } = null;
+        public List<string> Actors { get; set; }
+
+        public BsonDocument UnstructuredInfo { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# MoviesApi
+
+The following code demonstrates how to make a Web api with ASP.NET 6 and Mongodb.
